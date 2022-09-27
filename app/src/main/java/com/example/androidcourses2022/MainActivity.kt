cd62e4f66b5d72c3d464b707274a02ad113e1990
chepugash <chepugash@yahoo.com>
2022-09-27 09:17:51 +0300

    button edit added

--- app/src/main/java/com/example/androidcourses2022/MainActivity.kt
+++ app/src/main/java/com/example/androidcourses2022/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.androidcourses2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.androidcourses2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            btnEdit.setOnClickListener {
+                ivProfile.visibility = if (ivProfile.visibility == View.VISIBLE) {
+                    View.GONE
+                } else {
+                    View.VISIBLE
+                }
+            }
+        }
     }
 }

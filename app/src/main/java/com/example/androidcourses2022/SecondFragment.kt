1ad431670974a3de728f777a2e6581d5259df441
chepugash <chepugash@yahoo.com>
2022-10-11 04:07:51 +0300

    two buttons created

--- app/src/main/java/com/example/androidcourses2022/SecondFragment.kt
+++ app/src/main/java/com/example/androidcourses2022/SecondFragment.kt
@@ -1,4 +1,36 @@
 package com.example.androidcourses2022
 
-class SecondFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.androidcourses2022.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    private var binding: FragmentSecondBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSecondBinding.bind(view)
+
+        val counter =arguments?.getInt(ARG_NAME)
+        binding?.tvCounter?.text = "Counter value: $counter"
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    companion object {
+        private const val ARG_NAME = "counter"
+
+        fun newInstance(counter: Int) = SecondFragment().apply {
+            arguments = Bundle().apply {
+                putInt(ARG_NAME, counter)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/androidcourses2022/StartFragment.kt
+++ app/src/main/java/com/example/androidcourses2022/StartFragment.kt
@@ -1,4 +1,45 @@
 package com.example.androidcourses2022
 
-class StartFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.androidcourses2022.databinding.FragmentStartBinding
+
+class StartFragment : Fragment(R.layout.fragment_start) {
+
+    private var binding: FragmentStartBinding? = null
+
+    private var counter = 0
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentStartBinding.bind(view)
+
+        binding?.run {
+
+            btnFirst.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .setCustomAnimations(
+                        android.R.anim.fade_in,
+                        android.R.anim.fade_out,
+                        android.R.anim.fade_in,
+                        android.R.anim.fade_out,
+                    )
+                    .replace(R.id.container, SecondFragment.newInstance(counter))
+                    .addToBackStack("Start")
+                    .commit()
+            }
+
+            btnSecond.setOnClickListener {
+                counter++
+            }
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
 }

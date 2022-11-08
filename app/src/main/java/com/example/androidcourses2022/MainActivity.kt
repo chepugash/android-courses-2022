b65f16546b006761aaabe3e96a8330986c23e3ed
chepugash <chepugash@yahoo.com>
2022-11-08 23:45:13 +0300

    date and time pickers added

--- app/src/main/java/com/example/androidcourses2022/MainActivity.kt
+++ app/src/main/java/com/example/androidcourses2022/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.androidcourses2022
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidcourses2022.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    private var notificationProvider: NotificationProvider? = null
+
+    private val time = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        binding?.run {
+            tvTime.text = SimpleDateFormat("HH:mm").format(time.time)
+            tvDate.text = SimpleDateFormat("EE, dd MMMM").format(time.time)
+            tvTime.setOnClickListener {
+                val timeSetListener = TimePickerDialog
+                    .OnTimeSetListener { timePicker, hour, minute ->
+                        time.set(Calendar.HOUR_OF_DAY, hour)
+                        time.set(Calendar.MINUTE, minute)
+                        tvTime.text = SimpleDateFormat("HH:mm").format(time.time)
+                    }
+                TimePickerDialog(
+                    this@MainActivity,
+                    timeSetListener,
+                    time.get(Calendar.HOUR_OF_DAY),
+                    time.get(Calendar.MINUTE),
+                    true
+                ).show()
+            }
+
+            tvDate.setOnClickListener {
+                val dateSetListener = DatePickerDialog
+                    .OnDateSetListener { datePicker, year, month, dayOfMonth ->
+                        time.set(Calendar.YEAR, year)
+                        time.set(Calendar.MONTH, month)
+                        time.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                        tvDate.text = SimpleDateFormat("EE, dd MMMM").format(time.time)
+                    }
+                DatePickerDialog(
+                    this@MainActivity,
+                    dateSetListener,
+                    time.get(Calendar.YEAR),
+                    time.get(Calendar.MONTH),
+                    time.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
+            btnStart.setOnClickListener {
+                notificationProvider = NotificationProvider(this@MainActivity)
+                notificationProvider?.showNotification(
+                    title = "Проснись",
+                    text = "Тебе к первой паре..."
+                )
+
+            }
+
+            btnStop.setOnClickListener {
+                TODO()
+            }
+        }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        notificationProvider = null
+    }
+
 }
---
+++ app/src/main/java/com/example/androidcourses2022/NotificationProvider.kt
@@ -0,0 +1,59 @@
+package com.example.androidcourses2022
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class NotificationProvider(private val context: MainActivity) {
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun showNotification(title: String, text: String) {
+        val notificationManager:NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(context, MainActivity::class.java)
+        val pending = PendingIntent.getActivity(
+            context,
+            100,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT,
+        )
+
+        val builder: NotificationCompat.Builder =
+            NotificationCompat.Builder(
+                context,
+                context.getString(R.string.default_notification_channel_id)
+            )
+                .setSmallIcon(R.drawable.ic_baseline_access_alarms_24)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setAutoCancel(true)
+                .setContentIntent(pending)
+                .setCategory(NotificationCompat.CATEGORY_ALARM)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel(
+                context.getString(R.string.default_notification_channel_id),
+                context.getString(R.string.default_notification_channel_name),
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                lightColor = Color.GREEN
+                setShowBadge(true)
+            }.also {
+                notificationManager.createNotificationChannel(it)
+            }
+        } else {
+            builder.setLights(Color.GREEN, 100, 500)
+        }
+
+        notificationManager.notify(1111, builder.build())
+
+    }
+
+}
